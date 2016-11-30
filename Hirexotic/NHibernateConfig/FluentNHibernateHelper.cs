fda02bf1d08b33b35e7b517afaa43ad2dabe9286
AndradeD <danielqa17@gmail.com>
2016-11-30 17:06:59 -0200

    connectionDB

---
+++ Hirexotic/NHibernateConfig/FluentNHibernateHelper.cs
@@ -0,0 +1,23 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using Hirexotic.Models;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hirexotic.NHibernateConfig
+{
+    public static class FluentNHibernateHelper
+    {
+        public static ISession OpenSession()
+        {
+            string connectionString = "User ID=postgres;Password=;Host=localhost;Port=5432;Database=pcs-sgbd;Pooling=true;Min Pool Size=0;Max Pool Size=100;Connection Lifetime=0;";
+            ISessionFactory sessionFactory = Fluently.Configure().Database(PostgreSQLConfiguration.PostgreSQL82.ConnectionString(connectionString).ShowSql()).Mappings(m =>m.FluentMappings.AddFromAssemblyOf<Automovel>()).ExposeConfiguration(cfg => new SchemaExport(cfg).Create(false, false)).BuildSessionFactory();
+            return sessionFactory.OpenSession();
+        }
+
+    }
+}
---
+++ Hirexotic/Repositorio/Repositorio.cs
@@ -0,0 +1,89 @@
+﻿using Hirexotic.NHibernateConfig;
+using NHibernate;
+using NHibernate.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hirexotic.Repositorio
+{
+    public class Repositorio<T> where T : class
+    {
+        private static readonly ISessionFactory sessionFactory;
+        private ITransaction transaction;
+
+        public ISession Session { get; private set; }
+
+        public Repositorio()
+        {
+            Session = FluentNHibernateHelper.OpenSession();
+        }
+
+        public void BeginTransaction()
+        {
+            transaction = Session.BeginTransaction();
+        }
+
+        public void Commit()
+        {
+            try
+            {
+                // commit transaction if there is one active
+                if (transaction != null && transaction.IsActive)
+                    transaction.Commit();
+            }
+            catch
+            {
+                // rollback if there was an exception
+                if (transaction != null && transaction.IsActive)
+                    transaction.Rollback();
+
+                throw;
+            }
+            finally
+            {
+                Session.Dispose();
+            }
+        }
+
+        public void Rollback()
+        {
+            try
+            {
+                if (transaction != null && transaction.IsActive)
+                    transaction.Rollback();
+            }
+            finally
+            {
+                Session.Dispose();
+            }
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return Session.Query<T>();
+        }
+
+        public T GetById(int id)
+        {
+            return Session.Get<T>(id);
+        }
+
+        public void Create(T entity)
+        {
+            Session.Save(entity);
+        }
+
+        public void Update(T entity)
+        {
+            Session.Update(entity);
+        }
+
+        public void Delete(int id)
+        {
+            Session.Delete(Session.Load<T>(id));
+        }
+
+    }
+}
